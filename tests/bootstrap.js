7bda160d1094813d7cf2da80ed272f63dd00e9b1
Torf <mail@torf.cc>
2018-04-02 13:12:53 +0200

    Travis CI setup

--- tests/bootstrap.js
+++ tests/bootstrap.js
@@ -2,8 +2,21 @@ const puppeteer = require('puppeteer');
 
 const SHOW_BROWSER = false;
 
+const ON_TRAVIS = process.env.TRAVIS;
+
 before (async function () {
-    global.browser = await puppeteer.launch({devtools: SHOW_BROWSER});
+    let args = [];
+    if (ON_TRAVIS) {
+        // See https://github.com/GoogleChrome/puppeteer/blob/master/docs/troubleshooting.md#chrome-headless-fails-due-to-sandbox-issues
+        console.log('Disabling Chrome sandbox on Travis CI');
+        args.push('--no-sandbox');
+        args.push('--disable-setuid-sandbox');
+    };
+    let options = {
+        devtools: SHOW_BROWSER,
+        args: args,
+    };
+    global.browser = await puppeteer.launch(options);
     global.assert = require('chai').assert;
 });
 
